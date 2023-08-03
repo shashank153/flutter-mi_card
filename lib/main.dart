01745d06b0b5a7d9fdef0e1e007b6ec0c7a062fe
ShashanK Kumar Shukla <skshukl00@gmail.com>
2023-08-03 21:45:14 +0530

    mi card project done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,90 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 60.0,
+                foregroundImage: AssetImage('images/my_image.png'),
+              ),
+              Text(
+                'Shashank Kumar Shukla',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontFamily: 'Montez',
+                    fontWeight: FontWeight.w700),
+              ),
+              Text(
+                'SOFTWARE ENGINEER',
+                style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 16.0,
+                    fontFamily: 'Montserrat',
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  thickness: 1.0,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+91-8004829475',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Poppins',
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'skshukl00@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'Poppins',
+                        fontSize: 15.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
